2207cdfac3edebfbb17f8a95a2501040262026b1
Timo Volkmer <info@timovolkmer.com>
2025-12-10 20:07:05 +0100

    Updated to prisma ORM v7.1.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,21 @@
 import "dotenv/config";
-import { PrismaClient } from '../../prisma/src/generated/prisma/client.ts';
-import { DATABASE_URL } from '$env/static/private';
-import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaClient } from '../../prisma/src/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-const prisma = new PrismaClient({
-	datasourceUrl: DATABASE_URL
-}).$extends(withAccelerate());
+const connectionString = process.env.DATABASE_URL;
 
-export default prisma;
+// Create connection pool and the adapter
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+// Create prisma client singleton
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
+
+// 3. Global Object Handling (prevents hot-reload crashes)
+const globalForPrisma = global
+const prisma = globalForPrisma.prisma || prismaClientSingleton()
+export default prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
